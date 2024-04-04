0f1342889f8b722eedcce892137240ba8c2fe154
cassiegoe <cassiegoe@u.nus.edu>
2024-04-05 01:52:46 +0800

    added schoolenv app

---
+++ schoolenv_app/schoolenvapp.R
@@ -0,0 +1,67 @@
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(plotly)
+library(bslib)
+library(slickR)
+
+shooting_data <- read.csv("school-shootings-data-schoolenv.csv")
+df <- data.frame(shooting_data)
+
+incident_summary <- shooting_data %>%
+  group_by(school_type, resource_officer) %>%
+  summarize(total_incidents = n())
+
+# Define UI for application 
+ui <- fluidPage(
+  
+  tags$head(
+    tags$link(rel = "stylesheet", href = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css")
+  ),
+  
+  #school environment: section for school type & security
+  h4(strong("School Environment")),
+  fluidRow(
+    column(8, 
+           plotlyOutput("officerxtype_plot")),
+    column(4, 
+           p(style="text-align: justify; font-size = 20px",
+             "Insert elab"))
+  ),
+  br(),
+  card(
+    style = "background-color: white; font-family: Arial, sans-serif;", 
+    card_header(
+      class = "bg-dark text-white",
+      "What can be done?"
+    ),
+    markdown(
+      "Insert Solution")
+  ),
+  
+  
+  
+)
+
+
+# Define server logic required to draw 
+server <- function(input, output) {
+  
+  # officer x type plot
+  output$officerxtype_plot <- renderPlotly({
+    y <- ggplot(data = incident_summary, aes(x = school_type, y = total_incidents, fill = resource_officer)) +
+      geom_bar(stat = "identity", color = "black", position = position_dodge(width = 0.9)) +
+      labs(title = "School Type and Resource Officer Presence",
+           x = "School Type",
+           y = "Number of Incidents",
+           fill = "Presence of Resource Officer") +
+      theme_minimal()
+    ggplotly(y)
+  })
+  
+
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
